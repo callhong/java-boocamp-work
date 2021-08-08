c3fb2b044aca5703145f5039865997f9e33845c3
nanCheng <nancheng.lh@raycloud.com>
2021-08-09 00:01:27 +0800

    first work

---
+++ src/main/java/com/lh/javaboocampwork/JavaBoocampWorkApplication.java
@@ -0,0 +1,13 @@
+package com.lh.javaboocampwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBoocampWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBoocampWorkApplication.class, args);
+	}
+
+}
